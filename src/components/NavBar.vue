13d015a9f1c77d6ae1c41ed673d56209490bb1a5
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-05 13:39:03 -0500

    create project and navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import NavLink from "./NavLink.vue";
+import Logo from "/public/images/logo.png"
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg nav">
+    <img class="logo" alt="City Ground Logo" :src="Logo"/>
+
+      <div>
+        <ul class="nav-list">
+          <NavLink title="HOME" href="#"/>
+          <NavLink title="UPCOMING" href="#"/>
+          <NavLink title="RELEASES" href="#"/>
+          <NavLink title="CONTACT" href="#"/>
+        </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.nav {
+  background-color: black;
+  width: 100vw;
+}
+
+.nav-list {
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+.logo {
+  height: 100px;
+}
+
+</style>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <li class="nav-item">
+    <a :href="href" class="nav-link">
+      {{ title }}
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      required: true
+    },
+  }
+};
+</script>
+
+<style scoped>
+.nav-item{
+  list-style: none;
+}
+
+.nav-link{
+  color: white;
+  padding: 0 1rem;
+}
+
+.nav-link:hover {
+  color: rgb(0, 197, 255);
+}
+
+.nav-link.active {
+  color: rgb(0, 197, 255);
+}
+</style>
