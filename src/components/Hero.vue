0d00e634c98f71b2029422e5e4e0eed8a8b71b1f
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-05 16:14:31 -0500

    import remianing images

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,14 +2,16 @@
 import HeroImage from "./HeroImage.vue";
 import HeroMack from "/public/images/hero-mack.jpeg";
 import HeroAdam from "/public/images/hero-adam.jpeg";
+import HeroLiam from "/public/images/hero-liam.jpeg";
+import HeroNolan from "/public/images/hero-nolan.jpeg";
 </script>
 
 <template>
   <div class="container">
     <HeroImage :image-src="HeroMack" />
     <HeroImage :image-src="HeroAdam" />
-    <HeroImage :image-src="HeroMack" />
-    <HeroImage :image-src="HeroAdam" />
+    <HeroImage :image-src="HeroLiam" />
+    <HeroImage :image-src="HeroNolan" />
   </div>
 </template>
 
