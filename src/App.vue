ada46a0a2087fc628d6fe58875ef6c92d6afe3c3
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-05 17:28:51 -0500

    create instagram widget

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import NavBar from "./components/NavBar.vue";
 import Hero from "./components/Hero.vue"
 import Dates from "./components/Dates.vue";
+import InstaWidget from "./components/InstaWidget.vue";
 </script>
 
 <template>
   <NavBar />
   <Hero />
+  <InstaWidget />
   <Dates />
 </template>
 
---
+++ src/components/InstaWidget.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="widget-container">
+    <div class="elfsight-app-095efc21-5510-4de8-a33e-55ca4257504a" data-elfsight-app-lazy></div>
+  </div>
+</template>
+
+<style scoped>
+.widget-container {
+  width: 100vw;
+  margin-top: 20px;
+}
+</style>
