bba3815b39159c26a7bd5aaa5a448bbc37a83720
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-21 16:58:45 -0400

    add deploy script for gh actions

--- src/components/Dates.vue
+++ src/components/Dates.vue
@@ -11,13 +11,11 @@ const torontoLink = "https://www.eventbrite.com/e/out-of-the-blues-in-support-of
   <div class="dates-container">
     <h2>Upcoming Dates</h2>
     <Event title="White Rabbit" description="Join us at the White Rabbit for our next accoustic gig!"
-           month="March" day="8":link="whiteRabbitLink"/>
+           month="March" day="8" :link="whiteRabbitLink"/>
     <Event title="Room47" description="Join us at the Room47 to get your socks rocked off!" month="March" day="22"
            :link="room47Link"/>
     <Event title="Out of the Blues" description="Join us at our Toronto debut! Click the link for tickets."
            month="March" day="26" :link="torontoLink"/>
-  <Event title="Ryan Dooley" description="event details"
-  month="april" day="20" link="http://127.0.0.1:8000/dashboard" />
 </div>
 </template>
 
