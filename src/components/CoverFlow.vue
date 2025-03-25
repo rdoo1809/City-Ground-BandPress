ea19ac5037d3242ca1271e3e32a8ae98ff83d852
Ryan <r1d8o0o9@gmail.com>
2025-03-25 17:06:09 -0400

    Update CoverFlow.vue

--- src/components/CoverFlow.vue
+++ src/components/CoverFlow.vue
@@ -21,7 +21,7 @@ onMounted(() => {
         <Release :image="CoverOne" link="https://www.youtube.com/watch?v=fagEbM5qvCw" />
         <Release :image="CoverTwo" link="https://www.youtube.com/watch?v=fagEbM5qvCw" />
         <Release :image="CoverOne" link="https://www.youtube.com/watch?v=fagEbM5qvCw" />
-      <Release :image="/public/images/YqOpZbHbJq7kNf684kzxdDtHegvwyNpLMGUPlX6z.png" link="http://localhost:8000/dashboard" />
+      <Release image="/public/images/YqOpZbHbJq7kNf684kzxdDtHegvwyNpLMGUPlX6z.png" link="http://localhost:8000/dashboard" />
 </ul>
     </div>
   </div>
