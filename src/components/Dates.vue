cb0a2a695413bb3a94877c7c160343a69922a9b1
Ryan <r1d8o0o9@gmail.com>
2025-03-26 11:32:15 -0400

    Added new event to Dates.vue

--- src/components/Dates.vue
+++ src/components/Dates.vue
@@ -18,6 +18,8 @@ const torontoLink = "https://www.eventbrite.com/e/out-of-the-blues-in-support-of
            month="March" day="26" :link="torontoLink"/>
   <Event title="My New Event" description="This will be dope"
   month="April" day="20" link="http://localhost:8000/dashboard" />
+<Event title="Sweet Event" description="lorem ipsum"
+  month="April" day="21" link="http://localhost:8000/dashboard" />
 </div>
 </template>
 
