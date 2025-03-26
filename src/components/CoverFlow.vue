9fc13cf47accefed38b9e142af4ca839a4738102
Ryan <r1d8o0o9@gmail.com>
2025-03-26 10:46:10 -0400

    Added new Release to CoverFlow.vue

--- src/components/CoverFlow.vue
+++ src/components/CoverFlow.vue
@@ -21,7 +21,8 @@ onMounted(() => {
         <Release :image="CoverOne" link="https://www.youtube.com/watch?v=fagEbM5qvCw"/>
         <Release :image="CoverTwo" link="https://www.youtube.com/watch?v=fagEbM5qvCw"/>
         <Release :image="CoverOne" link="https://www.youtube.com/watch?v=fagEbM5qvCw"/>
-      </ul>
+      <Release image="/City-Ground/images/cO6NHCjNVbAMcxtuxhUK4Nr2rmarQ89LWjWk7wEW.png" link="http://localhost:8000/dashboard" />
+</ul>
     </div>
   </div>
 </template>
